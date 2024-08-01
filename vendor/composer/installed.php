eb0cc28c5f83545f9e145667fcdef6ef28c2fed2
Pedro Schinke <pedro.schinke@outlook.com>
2024-08-01 19:39:12 -0300

    composer installed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3bc62bc14974e89aab1481c584ceac8e9812f334',
+        'name' => 'dbseller/aluraplay',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dbseller/aluraplay' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3bc62bc14974e89aab1481c584ceac8e9812f334',
+            'dev_requirement' => false,
+        ),
+    ),
+);
