2c2f40d3cee8a8bbb52320d9cdb2beba2f729171
Pedro Schinke <pedro.schinke@outlook.com>
2024-08-07 18:44:26 -0300

    added error messages and secure file upload verification

--- src/Controller/EditVideoController.php
+++ src/Controller/EditVideoController.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Dbseller\Aluraplay\Controller;
 
+use Dbseller\Aluraplay\Domain\Helpers\StringHelper;
 use Dbseller\Aluraplay\Domain\Model\Video;
 use Dbseller\Aluraplay\Infra\Repository\PdoVideoRepository;
+use Dbseller\Aluraplay\Traits\FlashMessageTrait;
 
 class EditVideoController implements Controller
 {
+    use FlashMessageTrait;
     private PdoVideoRepository $videoRepository;
 
     public function __construct(PdoVideoRepository $videoRepository)
@@ -20,36 +23,46 @@ class EditVideoController implements Controller
     {
         $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
         if ($id === false || $id === null) {
-            header('Location: /?sucesso=0');
+            $this->addErrorMessage('Erro ao editar vídeo');
+            header("Location: /edit-video?id='$id'");
             return;
         }
 
         $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
         if ($url === false) {
-            header('Location: /?sucesso=0');
+            $this->addErrorMessage('URL inválida');
+            header("Location: /edit-video?id='$id'");
             return;
         }
 
         $title = filter_input(INPUT_POST, 'titulo');
         if ($title === false) {
-            header('Location: /?sucesso=0');
+            $this->addErrorMessage('É preciso informar um título');
+            header("Location: /edit-video?id='$id'");
             return;
         }
 
         $video = new Video($id, $url, $title);
 
         if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
-            move_uploaded_file(
-                $_FILES['image']['tmp_name'],
-                __DIR__ . '/../../public/img/uploads/' . $_FILES['image']['name']
-            );
-            $video->setFilePath($_FILES['image']['name']);
+            $finfo = new \finfo(FILEINFO_MIME_TYPE);
+            $mimeType = $finfo->file($_FILES['image']['tmp_name']);
+
+            if (StringHelper::startsWith($mimeType, 'image/')) {
+                $safeFileName = uniqid('upload_') . '_' . pathinfo($_FILES['image']['name'], PATHINFO_BASENAME);
+                move_uploaded_file(
+                    $_FILES['image']['tmp_name'],
+                    __DIR__ . '/../../public/img/uploads/' . $safeFileName
+                );
+                $video->setFilePath($safeFileName);
+            }
         }
 
         $success = $this->videoRepository->editVideo($video);
 
         if ($success === false) {
-            header('Location: /?sucesso=0');
+            $this->addErrorMessage('Erro ao editar vídeo');
+            header("Location: /edit-video?id='$id'");
         } else {
             header('Location: /?sucesso=1');
         }
--- src/Controller/NewVideoController.php
+++ src/Controller/NewVideoController.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Dbseller\Aluraplay\Controller;
 
+use Dbseller\Aluraplay\Domain\Helpers\StringHelper;
 use Dbseller\Aluraplay\Domain\Model\Video;
+use Dbseller\Aluraplay\Traits\FlashMessageTrait;
 use Dbseller\AluraPlay\Infra\Repository\PdoVideoRepository;
 
 class NewVideoController implements Controller
 {
+    use FlashMessageTrait;
     private PdoVideoRepository $videoRepository;
 
     public function __construct(PdoVideoRepository $videoRepository)
@@ -20,30 +23,39 @@ class NewVideoController implements Controller
     {
         $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
         if ($url === false) {
-            header('Location: /?sucesso=0');
+            $this->addErrorMessage('URL inválido');
+            header('Location: /new-video');
             return;
         }
 
         $title = filter_input(INPUT_POST, 'titulo');
         if ($title === false) {
-            header('Location: /?sucesso=0');
+            $this->addErrorMessage('É preciso informar um título');
+            header('Location: /new-video');
             return;
         }
 
         $video = new Video(null, $url, $title);
 
         if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
-            move_uploaded_file(
-                $_FILES['image']['tmp_name'],
-                __DIR__ . '/../../public/img/uploads/' . $_FILES['image']['name']
-            );
-            $video->setFilePath($_FILES['image']['name']);
+            $finfo = new \finfo(FILEINFO_MIME_TYPE);
+            $mimeType = $finfo->file($_FILES['image']['tmp_name']);
+
+            if (StringHelper::startsWith($mimeType, 'image/')) {
+                $safeFileName = uniqid('upload_') . '_' . pathinfo($_FILES['image']['name'], PATHINFO_BASENAME);
+                move_uploaded_file(
+                    $_FILES['image']['tmp_name'],
+                    __DIR__ . '/../../public/img/uploads/' . $safeFileName
+                );
+                $video->setFilePath($safeFileName);
+            }
         }
 
         $success = $this->videoRepository->saveVideo($video);
 
         if ($success === false) {
-            header('Location: /?sucesso=0');
+            $this->addErrorMessage('Erro ao cadastrar vídeo');
+            header('Location: /new-video');
         } else {
             header('Location: /?sucesso=1');
         }
